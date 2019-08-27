e3a0746d0e9dfd4f0500f77c66ceb3c3bff63307
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2019-08-27 14:09:08 +0500

    Updates

---
+++ precache-manifest.dc874166296935cc92b6225b974d55ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3f7e08f8cefb1c9931a6439a03fb54",
+    "url": "/react-app-gh-pages/index.html"
+  },
+  {
+    "revision": "0c375ad0ef3450c8f562",
+    "url": "/react-app-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3f82fb916c2ef8e69791",
+    "url": "/react-app-gh-pages/static/js/2.279448fb.chunk.js"
+  },
+  {
+    "revision": "0c375ad0ef3450c8f562",
+    "url": "/react-app-gh-pages/static/js/main.07dd9f74.chunk.js"
+  },
+  {
+    "revision": "7b94bef5e524e958683d",
+    "url": "/react-app-gh-pages/static/js/runtime~main.4c249a0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
